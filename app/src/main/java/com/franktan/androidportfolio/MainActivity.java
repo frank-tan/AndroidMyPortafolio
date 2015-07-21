58f8a0e822545679b191710b2f0466255d179f0f
Frank Tan <francis.tine@gmail.com>
2015-07-21 23:07:06 +0800

    Added a RecycleView with dummy CardViews

--- app/src/main/java/com/franktan/androidportfolio/MainActivity.java
+++ app/src/main/java/com/franktan/androidportfolio/MainActivity.java
@@ -1,17 +1,37 @@
 package com.franktan.androidportfolio;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    ProjectAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.project_card_list);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter
+        mAdapter = new ProjectAdapter(new String[] {"a", "b", "c", "d", "e"});
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
 
